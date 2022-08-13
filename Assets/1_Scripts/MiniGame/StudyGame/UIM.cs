185c51c4729350f07ba4137f736601b32d912afe
SeoHyun <namja4097@gmail.com>
2022-08-13 20:31:54 +0900

    경로 잘못한거 빠져서 고치고 오늘 한 거  - 널 레퍼런스

---
+++ Assets/1_Scripts/MiniGame/StudyGame/UIM.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIM : MonoBehaviour
+{
+    public GameObject panel;
+    public Text text;
+    int score = 0;
+
+
+
+    private void Update()
+    {
+         
+    }
+
+    private void Start()
+    {
+        SetText();
+    }
+
+    public void GetScore()
+    {
+        score += 100;
+        SetText();
+    }
+
+    public void SetText()
+    {
+        text.text = "Score : " + score.ToString();
+    }
+
+
+    public void OnClickPlay()
+    {
+        panel.SetActive(false);
+    }
+}
