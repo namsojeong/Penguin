1d38823822667378e8cef90fab63a7e9af0f1e45
SeoHyun <namja4097@gmail.com>
2022-08-21 23:41:47 +0900

    ㅊ

--- Assets/1_Scripts/AppScripts/ShopScript.cs
+++ Assets/1_Scripts/AppScripts/ShopScript.cs
@@ -5,26 +5,32 @@ using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
 {
-
+    public static ShopScript instance;
 
     public GameObject[] randomItem;
 
     public GameObject[] Item;
     public int[] itemPrice;
 
+    public AudioSource audioSource;
 
-    public Text talkText;
+
+    //public GameObject itemPosition;
 
 
-    public GameObject itemPosition;
+
+
+    public bool[] isBuyToItme;
 
 
     void Start()
     {
+
+        instance = this;
     }
     void Update()
     {
-        
+
     }
 
 
@@ -32,30 +38,45 @@ public class ShopScript : MonoBehaviour
     {
         int selection = Random.Range(0, randomItem.Length);
         GameObject selectedPrefab = randomItem[selection];
-        Instantiate(selectedPrefab, itemPosition.transform.position, Quaternion.identity);
+    //    Instantiate(selectedPrefab, itemPosition.transform.position, Quaternion.identity);
 
     }
 
     public void OnClickRandomItem()
     {
+        int gachaprice = 200;
+        GameManager.instance.coin -= gachaprice;
+
+        if (gachaprice > GameManager.instance.coin)
+        {
+            audioSource.Play();
+            return;
+        }
         RandomItem();
     }
 
-    public void Buy(int index)
+    public void OnclickBuy(int index)
     {
         int price = itemPrice[index];
-        if(price > GameManager.instance.coin)
+        if (price > GameManager.instance.coin)
         {
+            audioSource.Play();
             return;
         }
 
+        GameManager.instance.coin -= price;
+
+        bool isBuy = isBuyToItme[index];
+
+        isBuy = true;
+
 
     }
 
-    public void TalkText()
-    {
-        talkText.text = "";
-    }
+
+
+
+
 }
 
 
