41f2bfbc4e6c2001d73a4c9a09c8a14935268d25
SeoHyun <namja4097@gmail.com>
2022-08-25 18:45:27 +0900

    미니게임 버그 수정

---
+++ Assets/1_Scripts/MiniGame/StudyGame/Trash1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash1 : MonoBehaviour
+{
+
+    private float gravity = 9.8f;
+
+    private float mVelocity = 0f;
+
+    private PoolManager poolManager;
+
+    private void Awake()
+    {
+        poolManager = GetComponent<PoolManager>();
+    }
+
+    void Update()
+    {
+        Vector3 current = gameObject.transform.position;
+
+        mVelocity += gravity * Time.deltaTime;
+
+        current.y -= mVelocity * Time.deltaTime;
+        gameObject.transform.position = current;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Col")
+        {
+            //transform.SetParent(poolManager.transform, false);
+            //gameObject.SetActive(false);
+
+
+            Destroy(gameObject);
+
+        }
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            //transform.SetParent(poolManager.transform, false);
+            //gameObject.SetActive(false);
+        }
+    }
+
+
+}
